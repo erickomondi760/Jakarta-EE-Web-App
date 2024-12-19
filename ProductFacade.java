b34b5f0471429d267189ed4cf01c72147fb900c3
Erick <106428040+erickomondi760@users.noreply.github.com>
2024-12-19 06:57:50 +0300

    Update ProductService.java

--- ProductFacade.java
+++ ProductFacade.java
@@ -5,7 +5,6 @@
 package com.quickrest.facade;
 
 import com.quickrest.entities.Product;
-import com.quickrest.entities.Supplier;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
@@ -32,28 +31,6 @@ public class ProductFacade{
     public List<Product> findAll() {
         return entityManager.createQuery("select o from Product o").getResultList();
     }
-    
-    public List<Product> findProductBySupplier(String identity){
-        return entityManager.createNamedQuery(Product.FIND_PRODUCT_BY_SUPPLIER,Product.class)
-                .setParameter("identity", identity).getResultList();
-    }
-    
-    public List<Product> findProductByCode(String code){
-        return entityManager.createNamedQuery(Product.FIND_PRODUCT_BY_CODE,Product.class)
-                .setParameter("code", code).getResultList();
-    }
-    
-    public void deleteProduct(BigDecimal id){
-        entityManager.createNamedQuery(Product.DELETE_PRODUCT).setParameter("id", id);
-    }
-    
-    public List<Supplier> findSuppliers(){
-        return entityManager.createNamedQuery(Product.FIND_SUPPLIERS,Supplier.class).getResultList();
-    }
-
-   
-
-    
 
     
 }
